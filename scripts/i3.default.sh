2f3ba502a06fc841d00f7fd4a1f9d0e46f2e4956
dontdoxxmeplz <85412253+dontdoxxmeplz@users.noreply.github.com>
2021-11-22 00:08:07 -0500

    Update i3.default.sh

--- scripts/i3.default.sh
+++ scripts/i3.default.sh
@@ -102,20 +102,20 @@ sudo systemctl enable vmwareuser.service --now;
 fi
 
 cat << EOF > ~/.zprofile
-'
+"
 typeset -gU cdpath fpath mailpath path
 path=(
   /usr/local/{bin,sbin}
   $path
 )
-export LESS="-g -i -M -R -S -w -X -z-4"
+export LESS='-g -i -M -R -S -w -X -z-4'
 if (( $#commands[(i)lesspipe(|.sh)] )); then
-  export LESSOPEN="| /usr/bin/env $commands[(i)lesspipe(|.sh)] %s 2>&-"
+  export LESSOPEN='| /usr/bin/env $commands[(i)lesspipe(|.sh)] %s 2>&-'
 fi
 if [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then
     exec ssh-agent startx
 fi
-'
+"
 EOF
 
 chmod +x ~/.zprofile
